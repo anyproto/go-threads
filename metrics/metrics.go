9378d970a9d1ac8fdc98a5cd47ef40fb842965fc
mcrakhman <rakhmanov.m@gmail.com>
2021-11-14 18:58:14 +0100

    Add more metrics

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -12,11 +12,11 @@ const (
 
 type Metrics interface {
 	AcceptRecord(tp RecordType, isNAT bool)
-	CreateRecord(localEventBusMs int, pushRecordMs int)
+	CreateRecord(beforeMs int, localEventBusMs int, pushRecordMs int)
 }
 
 type NoOpMetrics struct{}
 
-func (n *NoOpMetrics) CreateRecord(localEventBusMs int, pushRecordMs int) {}
+func (n *NoOpMetrics) CreateRecord(beforeMs int, localEventBusMs int, pushRecordMs int) {}
 
 func (n *NoOpMetrics) AcceptRecord(tp RecordType, isNat bool) {}
