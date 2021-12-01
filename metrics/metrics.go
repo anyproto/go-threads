e27ec0f0019faecd6ddc30211d9a4c13f1a6e973
mcrakhman <rakhmanov.m@gmail.com>
2021-12-01 13:12:37 +0100

    Rename log edges to addr edges

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -13,13 +13,13 @@ const (
 type Metrics interface {
 	AcceptRecord(tp RecordType, isNAT bool)
 	CreateRecord(threadId string, prepareMs int64, newRecordMs int64, localEventBusMs int64, pushRecordMs int64)
-	DifferentLogEdges(localEdgeHash uint64, remoteEdgeHash uint64, peerId string, threadId string)
+	DifferentAddressEdges(localEdgeHash uint64, remoteEdgeHash uint64, peerId string, threadId string)
 	DifferentHeadEdges(localEdgeHash uint64, remoteEdgeHash uint64, peerId string, threadId string)
 }
 
 type NoOpMetrics struct{}
 
-func (n *NoOpMetrics) DifferentLogEdges(localEdgeHash uint64, remoteEdgeHash uint64, peerId string, threadId string) {}
+func (n *NoOpMetrics) DifferentAddressEdges(localEdgeHash uint64, remoteEdgeHash uint64, peerId string, threadId string) {}
 
 func (n *NoOpMetrics) DifferentHeadEdges(localEdgeHash uint64, remoteEdgeHash uint64, peerId string, threadId string) {}
 
